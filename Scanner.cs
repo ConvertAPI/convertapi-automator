864645a8ec0b722d7f2f938986f47bd55765863d
Tomas <tomas@baltsoft.com>
2020-01-21 15:39:28 +0200

    fix

--- Scanner.cs
+++ Scanner.cs
@@ -19,21 +19,18 @@ namespace convertapi_automator
 
         private static IEnumerable<ConvertApiFileParam> FileToParam(FileInfo f)
         {
+            var convertApiFileParams = new List<ConvertApiFileParam>();
+
             var format = Path.GetExtension(f.Name).Replace(".", "");
             if (format.Equals("zip", StringComparison.InvariantCultureIgnoreCase))
             {
-                using var zip = ZipFile.OpenRead(f.FullName);
-                // return zip.Entries.Select(f => new ConvertApiFileParam(f.Open(), f.Name));
-
-                var zs = zip.Entries.Select(f => (f.Open(), f.Name));
-
-                var fp = zs.Select(f => new ConvertApiFileParam(f.Item1, f.Name));
-
-                return new List<ConvertApiFileParam>();
-                // return fp;
+                using var zipa = ZipFile.OpenRead(f.FullName);
+                convertApiFileParams.AddRange(zipa.Entries.Select(zipArchiveEntry => new ConvertApiFileParam(zipArchiveEntry.Open(), zipArchiveEntry.Name)));
             }
+            else
+                convertApiFileParams.Add(new ConvertApiFileParam(f));
 
-            return new List<ConvertApiFileParam> { new ConvertApiFileParam(f) };
+            return convertApiFileParams;
         }
     }
 }
