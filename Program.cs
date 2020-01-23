70961918aac82f8579d635d17f3b780577164798
Jonas Jasas <jonas.jasas@gmail.com>
2020-01-23 16:21:07 +0200

    Description

--- Program.cs
+++ Program.cs
@@ -12,16 +12,23 @@ namespace convertapi_automator
     class Program
     {
         /// <summary>
+        /// <para>
         /// convertapi-automator provides easy way to convert your files to various formats.
-        /// Supported conversion types available available at: https://www.convertapi.com/doc/supported-formats
+        /// Supported conversion types available available at https://www.convertapi.com/doc/supported-formats .
+        /// </para>
         ///
-        /// convertapi-automator converts files located in conversion directory (set by --dir option).
-        /// Conversion destination format(s) are set by creating subdirectories inside conversion directory. 
+        /// <para>
+        /// convertapi-automator converts files located inside input directory (set by --dir option).
+        /// Conversion destination format(s) are set by creating subdirectories inside input directory named by format.
+        /// For example conv-dir/jpg all files placed inside conv-dir will be converted to jpg format and saved inside jpg directory.
+        /// Application can run in "watch" mode that will constantly monitor input directory for new files and convert them.
+        /// IMPORTANT: all files that are placed inside input directory will be deleted.
+        /// </para>
         /// 
         /// </summary>
         /// <param name="secret">Your convertapi.com secret.</param>
-        /// <param name="dir">Conversion directory</param>
-        /// <param name="watch">Watch conversion directory for new files and automatically convert</param>
+        /// <param name="dir">File input directory</param>
+        /// <param name="watch">Watch input directory for new files and automatically convert them</param>
         static int Main(string secret, string dir, bool watch = false)
         {
             var exitCode = 0;
@@ -32,12 +39,12 @@ namespace convertapi_automator
             }
             if (string.IsNullOrEmpty(dir))
             {
-                Console.Error.WriteLine("No conversion directory provided. Please set --dir option to a path of your conversion directory.");
+                Console.Error.WriteLine("No input directory provided. Please set --dir option to a path of your input directory.");
                 exitCode = 1;
             }
             else if (!Directory.Exists(dir))
             {
-                Console.Error.WriteLine("Conversion directory does not exist. Please set valid path to --dir option.");
+                Console.Error.WriteLine("Input directory does not exist. Please set valid path to --dir option.");
                 exitCode = 1;
             }
 
