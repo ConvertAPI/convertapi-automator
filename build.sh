0e6f0c0ca08545e56cece1bdd48a2ecca79f3dbb
Jonas Jasas <jonas.jasas@gmail.com>
2020-02-05 12:20:24 +0200

    Build script fix

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-mkdir -p bin/zip
+mkdir -p bin
+export GZIP=-9
 
-dotnet publish --configuration Release --runtime linux-x64 --self-contained --output bin/zip /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false
-cd bin/zip
-zip -9 -m -j convertapi-automator_linux.zip convertapi-automator
-cd ../..
+dotnet publish --configuration Release --runtime linux-x64 --self-contained --output=bin /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false /p:DebugSymbols=false /p:DebugType=None Cli
+cd bin
+mv Cli convertapi-automator
+tar --create --gzip --preserve-permissions --remove-files --file=convertapi-automator_linux.tar.gz convertapi-automator
+cd ..
 
-dotnet publish --configuration Release --runtime win-x64 --self-contained --output bin/zip /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false
-cd bin/zip
+dotnet publish --configuration Release --runtime osx-x64 --self-contained --output=bin /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false /p:DebugSymbols=false /p:DebugType=None Cli
+cd bin
+mv Cli convertapi-automator
+tar --create --gzip --preserve-permissions --remove-files --file=convertapi-automator_osx.tar.gz convertapi-automator
+cd ..
+
+dotnet publish --configuration Release --runtime win-x64 --self-contained --output=bin /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false /p:DebugSymbols=false /p:DebugType=None Cli
+cd bin
+mv Cli.exe convertapi-automator.exe
 zip -9 -m -j convertapi-automator_win.zip convertapi-automator.exe
-cd ../..
-
-dotnet publish --configuration Release --runtime osx-x64 --self-contained --output bin/zip /p:PublishReadyToRun=false /p:PublishSingleFile=true /p:PublishTrimmed=true /p:DebugBuild=false
-cd bin/zip
-zip -9 -m -j convertapi-automator_osx.zip convertapi-automator
-cd ../..
+cd ..
