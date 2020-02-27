889d74c3880afd1d3ee80c29bedad44f0813630c
Jonas Jasas <jonas@baltsoft.com>
2020-02-27 18:56:06 +0200

    Service installer

---
+++ WinConfig/Config.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WinConfig
+{
+    class Config
+    {
+        public const string ServiceName = "convertapi-automator";
+        public FileInfo ExeFile { get; set; }
+        public string Secret { get; set; }
+        public List<DirectoryInfo> Dirs { get; set; } = new List<DirectoryInfo>();
+        public int Level { get; set; } = 0;
+        public int Concurrency { get; set; } = 10;
+        public bool Autostart { get; set; } = true;
+        public string AutostartStr() => Autostart ? "auto" : "demand";
+
+        public Config(string secret, List<DirectoryInfo> dirs, int level, int concurrency)
+        {
+            Secret = secret;
+            Dirs = dirs;
+            Level = level;
+            Concurrency = concurrency;
+        }
+
+        public Config(IEnumerable<IEnumerable<string>> args)
+        {
+            foreach (var a in args) {
+                switch (a.First())
+                {
+                    case "secret":
+                        this.Secret = a.Last();
+                        break;
+                    case "dir":
+                        this.Dirs.Add(new DirectoryInfo(a.Last().Replace("'", "")));
+                        break;
+                    case "level":
+                        this.Level = int.Parse(a.Last());
+                        break;
+                    case "concurrency":
+                        this.Concurrency = int.Parse(a.Last());
+                        break;
+                }
+
+            }
+        }
+
+        public string Cmd()
+        {
+            var dirs = String.Join(" ", Dirs.Select(d => $"--dir='{d.FullName}'"));
+            return $"{ExeFile} --secret={Secret} --level={Level} --concurrency={Concurrency} {dirs}";
+        }
+
+    }
+}
---
+++ WinConfig/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace WinConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExecFileInput.Text = Path.Combine(Directory.GetCurrentDirectory(), "convertapi-automator.exe");
+                
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            // Display the command output.
+            Label1.Content = ServiceCmd();
+
+        }
+
+        private void DeleteService(Config config)
+        {
+            ExecSc($"delete {Config.ServiceName}");
+        }
+
+        private void StopService(Config config)
+        {
+            ExecSc($"stop {Config.ServiceName}");
+        }
+
+        private void StartService(Config config)
+        {
+            ExecSc($"start {Config.ServiceName}");
+        }
+
+        private void UpdateService(Config config)
+        {
+            ExecSc($"config {Config.ServiceName} start={(config.Autostart ? "auto" : "demand")} binPath={config.Cmd()}");
+        }
+
+        private void RegisterService(Config config)
+        {
+            ExecSc($"create {Config.ServiceName} start={config.AutostartStr()} binPath={config.Cmd()}");
+        }
+
+        private string ServiceCmd()
+        {
+            var cmdOut = ExecSc($"qc {Config.ServiceName}");
+            return cmdOut.Single(l => l.Contains("BINARY_PATH_NAME")).Split(" : ").Last().Trim();
+        }
+
+        private IEnumerable<string> ExecSc(string args)
+        {
+            var procStartInfo = new System.Diagnostics.ProcessStartInfo("sc.exe", args);
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+            var proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            var cmdOut = proc.StandardOutput.ReadToEnd().Split("\r\n");
+            if (proc.ExitCode != 0) throw new Exception(proc.StandardError.ReadToEnd());
+            return cmdOut;
+        }
+
+        private IEnumerable<IEnumerable<string>> ParseArgs(string cmdLine)
+        {
+            return cmdLine.Split("--").Skip(1)
+                .Select(p =>
+                    p.Trim().Split("=").Select(i => i.Trim())
+                );
+        }
+
+        private void SelectExeFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "convertapi-automator.exe|convertapi-automator.exe";
+            if (openFileDialog.ShowDialog() == true)
+                ExecFileInput.Text = openFileDialog.FileName;
+        }
+
+        private void AddDirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog() { IsFolderPicker = true };
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                DirListBox.Items.Add(dialog.FileName);
+            }
+        }
+    }
+}
